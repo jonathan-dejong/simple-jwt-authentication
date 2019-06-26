a9099af3a4f3943790549b585ba239acc478351b
Jan Thiel <jan@hive-it.de>
2019-06-26 15:27:21 +0200

    Only display the JWT Secret, if it is configured as a site option. If it is set globally in wp-config, do not display it within the backend. Knowing the signing secret might lead to security issues in a multisite setup with a global signing secret.

--- includes/admin/views/settings/secret-key.php
+++ includes/admin/views/settings/secret-key.php
@@ -1,8 +1,16 @@
-<input type="text" name='simple_jwt_authentication_settings[secret_key]' value='<?php echo $secret_key; ?>' <?php echo ( $is_global ? 'readonly' : '' ); ?> size="50" autocomplete="off" />
 <?php
+
+// Only display the Secret key, if it is not set in the wp-config file.
+// In Multisite Installations it could be a security issue to display a global secret to other sites admins
 if ( $is_global ) {
+    ?>
+    <input type="text" value="<?php _e('-- HIDDEN --', 'simple-jwt-authentication'); ?>" readonly="readonly" size="50" autocomplete="off" />
+    <?php
 	echo '<br /><small>' . __( 'Defined in wp-config.php', 'simple-jwt-authentication' ) . '</small>';
 } else {
+    ?>
+    <input type="text" name='simple_jwt_authentication_settings[secret_key]' value='<?php echo $secret_key; ?>' size="50" autocomplete="off" />
+    <?php
 	echo '<br /><small>' . __( 'Should be a long string of letters, numbers and symbols.', 'simple-jwt-authentication' ) . '</small>';
 }
 ?>
